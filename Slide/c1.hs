bd1aeb54f7650d4a059ac46233918c87f512e0ed
Trevor Devalle <trevor.devalle@gmail.com>
2017-07-05 02:01:16 +0200

    added es1

---
+++ Slide/c1.hs
@@ -0,0 +1,52 @@
+module Slide1 where
+
+{-- 
+
+	Author: Devalle Trevor
+	Number: 1/ 17
+
+--}
+
+{-- 
+
+1) Show that sum [x]=x for any number x.
+
+--}
+
+mysum       :: [Int] -> Int
+mysum []     = 0
+mysum (x:xs) = x + mysum xs
+
+{-- 
+
+	2) Define a function product that produces the product of a list of numbers, 
+	and show using your definition that product [ 2, 3, 4 ] = 24. (See attachment folder)
+
+--}
+
+myproduct       :: [Int] -> Int
+myproduct []     = 1
+myproduct (x:xs) = x * myproduct xs
+
+
+{-- 
+
+	3) How should the definition of the function qsort be modified so that it 
+ 	produces a reverse sorted version of a list?
+
+--}
+ 
+qsort1         :: Ord a => [a] -> [a]
+qsort1 []       = []
+qsort1 (x : xs) = qsort1 smaller ++ [x] ++ qsort1 larger
+               where
+                   smaller = [a | a <- xs, a > x ]
+                   larger  = [b | b <- xs, b <= x ]
+
+{-- 
+	4) What would be the effect of replacing <= by < in the definition of qsort?
+
+
+	Answer: An ordered list without duplicates.
+
+--}
